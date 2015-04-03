8869a5ef20078c98fe9143ab2066364056945ce8
Zyrst <zajreg@gmail.com>
2015-04-03 12:57:08 +0200

    Konst

---
+++ Above/Source/Above/ConstValues.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Above.h"
+#include "ConstValues.h"
+
+ConstValues::ConstValues()
+{
+}
+
+ConstValues::~ConstValues()
+{
+}
---
+++ Above/Source/Above/ConstValues.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class ABOVE_API ConstValues
+{
+public:
+	ConstValues();
+	~ConstValues();
+};
