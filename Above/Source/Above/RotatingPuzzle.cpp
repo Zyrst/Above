06b1a09cd02547555e4b24a1896214715b773c84
Zyrst <zajreg@gmail.com>
2015-04-07 17:05:25 +0200

    Door with comments, start on rotating puzzle

--- Above/Source/Above/RotatingPuzzle.cpp
+++ Above/Source/Above/RotatingPuzzle.cpp
@@ -1,65 +1,73 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Above.h"
-#include "Door.h"
+#include "RotatingPuzzle.h"
 
 
 // Sets default values
-ADoor::ADoor()
+ARotatingPuzzle::ARotatingPuzzle()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	RotationDegree = 90;
+
 }
 
 // Called when the game starts or when spawned
-void ADoor::BeginPlay()
+void ARotatingPuzzle::BeginPlay()
 {
 	Super::BeginPlay();
+	mRotate = false;
+
 	TArray<UStaticMeshComponent*> Components;
 	this->GetComponents<UStaticMeshComponent>(Components);
 	for (int32 i = 0; i < Components.Num(); i++){
 		UStaticMeshComponent* mesh = Components[i];
-		if (mesh->GetName() == "Door"){
-			mDoorMesh = mesh->StaticMesh;
-			mDoor = Components[i];
-			GEngine->AddOnScreenDebugMessage(-1, 2.0, FColor::Green, TEXT("Able to find mesh"));
+		if (mesh->GetName() == "Disk"){
+			mDishMesh = Components[i];
+
 		}
-		
 	}
-	//Two * Pi * R
-	mBase = ((2 * 3.14) * mDoorMesh->GetBounds().SphereRadius);
-	
+	mBase = (2 * 3.14) * mDishMesh->StaticMesh->GetBounds().SphereRadius;
+	for (int32 i = 0; i < 6; i++){
+		if (i == 0)
+			mPoints.Push(0);
+		else
+			mPoints.Push(i * 60);
+	}
+	for (int32 j = 0; j < mPoints.Num(); j++){
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Magenta, FString::Printf(TEXT("Number %f"), mPoints[j]));
+	}
+	Activate(90);
 }
 
 // Called every frame
-void ADoor::Tick( float DeltaTime )
+void ARotatingPuzzle::Tick( float DeltaTime )
 {
 	Super::Tick( DeltaTime );
-	if (mShouldRotate){
+
+	if (mRotate){
+		//Do stuff
 		//(two*pi*r*(CurrentDegree / 360))
 		if ((mBase * (mCurrent / 360)) <= mTarget){
 			//GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Blue, FString::Printf(TEXT("Degree %f"), ((mBase * (mCurrent / 360)))));
-			mDoor->AddLocalRotation(FRotator::FRotator(0, 1, 0), false, nullptr);
+			//mDishMesh->AddLocalRotation(FRotator::FRotator(0, 1, 0), false, nullptr);
+			this->AddActorLocalRotation(FRotator(0, 1, 0), false, nullptr);
 			mCurrent += 1.0;
 		}
-		
+
 		if (mBase * (mCurrent / 360) >= mTarget){
-			mShouldRotate = false;
+			mRotate = false;
 			Reset();
 		}
 	}
 }
 
-void ADoor::Activate(){
-	mShouldRotate = true;
-	float target = (RotationDegree / 360);
-	mTarget = mBase * target;
-	GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Black, FString::Printf(TEXT("Size %f"), mTarget));
-	//GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Black, FString::Printf(TEXT("BlueprintDegree %f"), RotationDegree));
+void ARotatingPuzzle::Activate(float target){
+	mRotate = true;
+	mTarget = mBase * (target / 360);
 }
 
-void ADoor::Reset(){
+void ARotatingPuzzle::Reset(){
 	mCurrent = 0;
 	mTarget = 0;
 }
