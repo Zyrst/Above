fdf5f53eba0f1c6423fec30c884171a63180edae
Zyrst <zajreg@gmail.com>
2015-05-05 15:23:09 +0200

    EndTrigger transitions into Credits

---
+++ Above/Source/Above/Credits.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "Credits.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ABOVE_API ACredits : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
