63df2deece899c1083cebf91e9028a9e9121103f
Zyrst <zajreg@gmail.com>
2015-05-12 09:16:12 +0200

    Settings

---
+++ Above/Source/Above/PlayerSetting.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Above.h"
+#include "PlayerSetting.h"
+
+void APlayerSetting::OnConstruction(const FTransform& Transform){
+	mSettings = GEngine->GameUserSettings;
+}
+
+void APlayerSetting::SetWindow(){
+
+	switch (WindowMode){
+	case 0:
+		if (!(mSettings->GetScreenResolution().X == Resolution.X) && !(mSettings->GetScreenResolution().Y == Resolution.Y))
+			mSettings->SetScreenResolution(FIntPoint(Resolution.X, Resolution.Y));
+		if (!(mSettings->GetFullscreenMode() == EWindowMode::Fullscreen))
+			mSettings->SetFullscreenMode(EWindowMode::Fullscreen);
+		break;
+	case 1:
+		if (!(mSettings->GetScreenResolution().X == Resolution.X) && !(mSettings->GetScreenResolution().Y == Resolution.Y))
+			mSettings->SetScreenResolution(FIntPoint(Resolution.X, Resolution.Y));
+		if (!(mSettings->GetFullscreenMode() == EWindowMode::Windowed))
+			mSettings->SetFullscreenMode(EWindowMode::Windowed);
+		break;
+	case 2:
+		if (!(mSettings->GetScreenResolution().X == Resolution.X) && !(mSettings->GetScreenResolution().Y == Resolution.Y))
+			mSettings->SetScreenResolution(FIntPoint(Resolution.X, Resolution.Y));
+		if (!(mSettings->GetFullscreenMode() == EWindowMode::WindowedFullscreen))
+			mSettings->SetFullscreenMode(EWindowMode::WindowedFullscreen);
+		break;
+	}
+
+	mSettings->ConfirmVideoMode();
+	mSettings->ApplyResolutionSettings(true);
+	mSettings->ApplyNonResolutionSettings();
+	mSettings->ApplySettings(true);
+	mSettings->SaveSettings();
+}
+
+void APlayerSetting::ToggleVSync(bool value){
+	mSettings->SetVSyncEnabled(value);
+
+	VSYNC = value;
+	mSettings->ApplyNonResolutionSettings();
+
+	mSettings->ApplySettings(true);
+	mSettings->SaveSettings();
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("VSYNC value %s"), VSYNC ? TEXT("true") : TEXT("false")));
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("VSYNC Gamesettings %s"), mSettings->IsVSyncEnabled() ? TEXT("true") : TEXT("false")));
+}
---
+++ Above/Source/Above/PlayerSetting.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Info.h"
+#include "PlayerSetting.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ABOVE_API APlayerSetting : public AInfo
+{
+	GENERATED_BODY()
+	
+public:
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	UFUNCTION(BlueprintCallable, Category = Window)
+		void SetWindow();
+	UFUNCTION(BlueprintCallable, Category = VSync)
+		void ToggleVSync(bool value);
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Screen)
+		FVector2D Resolution;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Screen)
+		int32 WindowMode;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = VSync)
+		bool VSYNC;
+private:
+	UGameUserSettings* mSettings;
+
+	bool onceLoad = false;
+};
